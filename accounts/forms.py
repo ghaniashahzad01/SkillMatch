07322aa6d7a8cfc86d17a7317c78cfcaba0020d2
Ghania Shahzad <ghaniashahzad287@gmail.com>
2026-01-03 21:57:52 +0500

    Created a custom user.

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import User
+
+class EmployerRegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        user.is_employer = True
+        if commit:
+            user.save()
+        return user
