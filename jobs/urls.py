aa51c9f7d8e9a9d1869debcd79b459447244907c
Ghania Shahzad <ghaniashahzad287@gmail.com>
2026-01-03 23:49:24 +0500

    Implement CRUD.

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import create_job
+from .views import create_job, edit_job, toggle_job_status, delete_job
 
 urlpatterns = [
     path('create/', create_job, name='job-create'),
+    path('edit/<int:job_id>/', edit_job, name='job-edit'),
+    path('toggle/<int:job_id>/', toggle_job_status, name='job-toggle'),
+    path('delete/<int:job_id>/', delete_job, name='job-delete'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from .forms import JobForm
+from .models import Job
 
 
 @login_required
@@ -19,3 +21,47 @@ def create_job(request):
         form = JobForm()
 
     return render(request, 'jobs/create_job.html', {'form': form})
+
+
+
+@login_required
+def edit_job(request, job_id):
+    job = get_object_or_404(Job, id=job_id, employer=request.user)
+
+    if request.method == 'POST':
+        form = JobForm(request.POST, instance=job)
+        if form.is_valid():
+            form.save()
+            return redirect('employer-dashboard')
+    else:
+        form = JobForm(instance=job)
+
+    return render(request, 'jobs/edit_job.html', {
+        'form': form,
+        'job': job
+    })
+
+
+@login_required
+def toggle_job_status(request, job_id):
+    job = get_object_or_404(Job, id=job_id, employer=request.user)
+
+    job.is_active = not job.is_active
+    job.save()
+
+    return redirect('employer-dashboard')
+
+
+
+
+@login_required
+def delete_job(request, job_id):
+    job = get_object_or_404(Job, id=job_id, employer=request.user)
+
+    if request.method == 'POST':
+        job.delete()
+        return redirect('employer-dashboard')
+
+    return render(request, 'jobs/delete_job_confirm.html', {
+        'job': job
+    })
