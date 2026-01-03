e79cf16fc393e80cccddb6be1f3714d0da39904b
Ghania Shahzad <ghaniashahzad287@gmail.com>
2026-01-03 22:32:20 +0500

    Added login page.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import employer_register
+from .views import employer_register, login_view
 
 urlpatterns = [
     path('register/employer/', employer_register, name='employer-register'),
+    path('login/', login_view, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .forms import EmployerRegisterForm
 
 def employer_register(request):
@@ -7,3 +8,21 @@ def employer_register(request):
         form.save()
         return redirect('login')
     return render(request, 'accounts/employer_register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            if user.is_employer:
+                return redirect('employer-dashboard')
+
+            return redirect('home')
+
+    return render(request, 'accounts/login.html')
